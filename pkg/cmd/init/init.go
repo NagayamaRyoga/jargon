14db13107985bd9fbbac243de063700f9df338e2
NagayamaRyoga <nagayama15@sec.is.kit.ac.jp>
2023-03-01 00:21:41 +0900

    feat: implement async prepare

--- pkg/cmd/init/init.go
+++ pkg/cmd/init/init.go
@@ -1,3 +1,4 @@
+//go:generate curl -sSL -o async.zsh https://raw.githubusercontent.com/mafredri/zsh-async/main/async.zsh
 package initialize
 
 import (
@@ -8,10 +9,14 @@ import (
 type Args struct {
 }
 
+//go:embed async.zsh
+var asyncZsh string
+
 //go:embed init.zsh
 var initZsh string
 
 func Run(*Args) error {
+	fmt.Println(asyncZsh)
 	fmt.Print(initZsh)
 	return nil
 }
--- pkg/cmd/init/init.zsh
+++ pkg/cmd/init/init.zsh
@@ -1,18 +1,34 @@
+jargon_prepare_async_callback_git() {
+  JARGON_GIT_INFO="$3"
+  zle reset-prompt
+}
+
+jargon_prepare_async() {
+  local source="$1"
+  local worker="jargon_async_worker_$source"
+  async_stop_worker "$worker"
+  async_start_worker "$worker" -n
+  async_register_callback "$worker" "jargon_prepare_async_callback_$source"
+  async_job "$worker" jargon prepare --source="$source"
+}
+
 jargon_chpwd() {
+  unset JARGON_GIT_INFO
 }
 
 jargon_preexec() {
+  unset JARGON_EXIT_STATUS_OVERWRITE
   JARGON_START="${EPOCHREALTIME}"
 }
 
 jargon_precmd() {
-  JARGON_GIT_INFO="$(jargon prepare --source=git)"
-
-  JARGON_EXIT_STATUS="$?"
+  JARGON_EXIT_STATUS="${JARGON_EXIT_STATUS_OVERWRITE:-$?}"
   JARGON_JOBS="${#jobstates}"
   local end="${EPOCHREALTIME}"
   JARGON_DURATION="$((${end} - ${JARGON_START:-${end}}))"
   unset JARGON_START
+
+  jargon_prepare_async git
 }
 
 jargon_prompt() {
@@ -22,6 +38,14 @@ jargon_prompt() {
 jargon_rprompt() {
   jargon prompt --right --exit-status="${JARGON_EXIT_STATUS}" --duration="${JARGON_DURATION}" --jobs="${JARGON_JOBS}" --width="$COLUMNS" --data-git="$JARGON_GIT_INFO"
 }
+
+jargon_clear_screen() {
+  JARGON_EXIT_STATUS_OVERWRITE=0
+  jargon_precmd
+  zle .clear-screen
+}
+
+zle -N clear-screen jargon_clear_screen
 
 autoload -Uz add-zsh-hook
 add-zsh-hook chpwd jargon_chpwd
